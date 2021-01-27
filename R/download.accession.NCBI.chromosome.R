d53de749b87de5b90cd5bcf51ace6ec829757d92
Maxime Bonjean <m.bonjean@student.uclouvain.be>
2021-01-27 16:39:39 +0100

    probleme sur le download accession

--- R/download.accession.NCBI.chromosome.R
+++ R/download.accession.NCBI.chromosome.R
@@ -12,14 +12,15 @@ download.accession.NCBI.chromosome <- function(accessionDir)
 {
   library(Biostrings)
   library(reutils)
+  library(seqinr)
   for (i in 1:2) {
 
-  demo.search <- esearch(term = paste0("Vibrio cholerae[orgn] and chromosome ",i,"[title] and complete [title]"), db = 'nuccore', usehistory = TRUE) #search
-  accessions <- efetch(demo.search, rettype = "acc",retmode = "text",outfile= paste0(accessionDir,species[i],".csv"))#fetch accessions
-  accession  <- read.csv(paste0('01-accession-list/',species[i],".csv"),header=F,stringsAsFactors = F)
-  accession$V1 <- unlist(lapply(strsplit(accession$V1,split='\\.'),function(x) x[[1]]))
-  write.csv(accession,paste0(accessionDir,'cholerae-',i,".csv"),row.names = F)
-  print(i)
+    demo.search <- esearch(term = paste0("Vibrio cholerae[orgn] and chromosome ",i," [title] and complete [title]"), db = 'nuccore', usehistory = TRUE) #search
+    accessions <- efetch(demo.search, rettype = "acc",retmode = "text",outfile= paste0(accessionDir,'vcholerae_c',i,".csv"))#fetch accessions
+    accession  <- read.csv(paste0(accessionDir,"vcholerae_c",i,".csv"),header=F,stringsAsFactors = F)
+    accession$V1 <- unlist(lapply(strsplit(accession$V1,split='\\.'),function(x) x[[1]]))
+    write.csv(accession,paste0(accessionDir,'vcholerae_c',i,".csv"),row.names = F)
+    print(i)
 
-}
+  }
 }
