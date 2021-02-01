6c218b11b1b8e916351edcaff03c1d19419ef12b
Maxime Bonjean <m.bonjean@student.uclouvain.be>
2021-02-01 18:49:28 +0100

    Ajout de phylo

---
+++ R/Phylo.R
@@ -0,0 +1,31 @@
+#' fait un arbre phylogenetic. Utiliser avec de grand nombre de sequences
+#'
+#' @param bacteria.table
+#'
+#' @return
+#' @export
+#'
+#' @examples
+phylo <- function(sequences)
+{
+  library(dplyr)
+  library(Biostrings)
+  library(muscle)
+  library(phangorn)
+  library(ggtree)
+  maxlength <- max(width(sequences))
+
+  align.muscle <- muscle::muscle(sequences)
+  dist1 <- stringDist(as(align.muscle,"DNAStringSet"), method="hamming")
+  dist1 <- 100*dist1/maxlength
+  mytree1 <- upgma(dist1)
+
+  nseqeuences <- length(sequences)
+
+  pdf(paste0('phylogenetic.otus.pdf'),width=10,height = 10*log10(nseqeuences))
+  p <- ggtree(mytree1)
+  p <- p  + geom_tiplab(offset=0) + xlim(NA, 150) +geom_treescale(0.05,-2.5,width=10,fontsize = 2,linesize = 0.5)
+  plot(p)
+  dev.off()
+
+}
