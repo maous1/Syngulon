03bc961028739115e999f5a7a1b7b66ae3560b00
Maxime Bonjean <m.bonjean@student.uclouvain.be>
2021-02-01 19:39:56 +0100

    phylo

--- R/Phylo.R
+++ R/Phylo.R
@@ -1,6 +1,7 @@
 #' fait un arbre phylogenetic. Utiliser avec de grand nombre de sequences
 #'
 #' @param bacteria.table
+#' @param Dir
 #'
 #' @return
 #' @export
@@ -8,8 +9,6 @@
 #' @examples
 phylo <- function(sequences)
 {
-  library(dplyr)
-  library(Biostrings)
   library(muscle)
   library(phangorn)
   library(ggtree)
@@ -27,5 +26,4 @@ phylo <- function(sequences)
   p <- p  + geom_tiplab(offset=0) + xlim(NA, 150) +geom_treescale(0.05,-2.5,width=10,fontsize = 2,linesize = 0.5)
   plot(p)
   dev.off()
-
 }
